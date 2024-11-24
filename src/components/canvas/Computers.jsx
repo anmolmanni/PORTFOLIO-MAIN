640addc8eff7b3300b1d9cfa5f60b205fcbeacca
anmolmanni <mannianmol@gmail.com>
2024-11-24 15:01:32 -0500

    Added 3d desktop

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,31 +10,34 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black"/>
+      <hemisphereLight intensity={0.15} 
+      groundColor="black" />
       <pointLight intensity={1}/>
       <primitive
         object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
       />
     </mesh>
   )
 }
 
-const ComputersCanvas=()=>{
+const ComputersCanvas =()=>{
   return (
     <Canvas
-    frameLoop='demand'
+    frameLoop="demand"
     shadows
     camera={{position: [20,3,5], fov:25 }}
     gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls 
-          enableZoom={false}
-          maxPoLarAngle={Math.PI / 2}
-          minPoLarAngle={Math.PI / 2}
+          enableZoom={true}
+          maxPoLarAngle={Math.PI/2}
+          minPoLarAngle={Math.PI/2}
         />
-        <Computers/>
-      </Suspense>
+        < Computers />
+      </Suspense >
 
       <Preload all />
     </Canvas>
