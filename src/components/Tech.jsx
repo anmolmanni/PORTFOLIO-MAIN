e3555ff34619e43e70a3153a52e9d9f2953f1ede
anmolmanni <75797203+anmolmanni@users.noreply.github.com>
2025-04-07 17:02:21 +0000

    main

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,7 @@ const Tech = () => {
     <div className="flex flex-wrap flex-row justify-center gap-10">
       {technologies.map((technology)=>(
         <div className="w-28 h-28" key={technology.name}>
-          {/* <BallCanvas icon={technology.icon}/> */}
+          <BallCanvas icon={technology.icon}/>
         </div>
       ))}
     </div>
